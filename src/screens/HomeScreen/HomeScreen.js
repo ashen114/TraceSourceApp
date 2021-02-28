4b4729d5eeaf9628dfa505025848e306bbaf1612
unknown <1215772764@qq.com>
2021-02-28 19:40:15 +0800

    :construction:feat: add contacts&chart

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,153 @@
+import React, {useState, useLayoutEffect, useCallback, useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid,
+  Dimensions,
+} from 'react-native';
+import {
+  NoticeBar,
+  Accordion,
+  List,
+  Card,
+  WhiteSpace,
+  WingBlank,
+} from '@ant-design/react-native';
+import Contacts from 'react-native-contacts';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+
+const HomeScreen = () => {
+  const [contactList, setContactList] = useState([]);
+  useLayoutEffect(() => {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+      title: 'Contacts',
+      message: 'This app would like to view your contacts.',
+      buttonPositive: 'Please accept bare mortal',
+    })
+      .then(() => Contacts.getAll())
+      .then((contacts) => {
+        console.log('contacts:', contacts);
+        setContactList(contacts);
+      });
+  }, []);
+
+  // const [echartOptions, setEchartOptions] = useState({
+  //   xAxis: {
+  //     type: 'category',
+  //     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  //   },
+  //   yAxis: {
+  //     type: 'value',
+  //   },
+  //   series: [
+  //     {
+  //       data: [820, 932, 901, 934, 1290, 1330, 1320],
+  //       type: 'line',
+  //     },
+  //   ],
+  // });
+  const data = [
+    [
+      [0, 1],
+      [1, 3],
+      [3, 7],
+      [4, 9],
+    ],
+  ];
+
+  return (
+    <View>
+      <NoticeBar mode="closable" onPress={() => alert('will close')}>
+        Notice: The arrival time of incomes and transfers of Yu 'E Bao will be
+        delayed during National Day.
+      </NoticeBar>
+      <WhiteSpace size="lg" />
+      <WingBlank size="lg">
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width - 30} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: '#fb8c00',
+            backgroundGradientTo: '#ffa726',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '6',
+              strokeWidth: '2',
+              stroke: '#ffa726',
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </WingBlank>
+      <WhiteSpace size="lg" />
+      <WingBlank size="lg">
+        {contactList.map((item, index) => {
+          return (
+            <Card key={index}>
+              <Card.Header
+                title={item.displayName}
+                thumbStyle={{width: 30, height: 30}}
+                thumb={item.thumbnailPath}
+                extra=""
+              />
+              <Card.Body>
+                <View>
+                  {item.phoneNumbers.map((phoneItem, phoneIndex) => {
+                    return (
+                      <Text key={phoneIndex} style={{marginLeft: 16}}>
+                        {phoneItem.label}: {phoneItem.number}
+                      </Text>
+                    );
+                  })}
+                </View>
+              </Card.Body>
+              <Card.Footer
+                content="footer content"
+                extra="footer extra content"
+              />
+            </Card>
+          );
+        })}
+      </WingBlank>
+    </View>
+  );
+};
+
+export default HomeScreen;
